d14cfaa32abc805dd98b2d053d28d49856c39ccd
Ethan Gottlieb <ethan@dormserv.com>
2014-09-27 15:30:58 -0400

    added driver login

--- dormserv/drivers/urls.py
+++ dormserv/drivers/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 from drivers import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.driver_dash, name='driver_dash'),
+    url(r'^$', views.driver_login, name='driver_login'),
+	url(r'^driver_dash', views.driver_dash, name='driver_dash'),
 
 
     )
--- dormserv/drivers/views.py
+++ dormserv/drivers/views.py
@@ -7,6 +7,77 @@ from drivers.models import DriverProfile
 from users.models import User, UserProfile
 # Create your views here.
 
+
+
+def user_login(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+    cur_usr = request.user
+
+    if cur_usr.is_active:
+        if is_driver(cur_usr):
+            return redirect('/drivers', user=cur_usr)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user is not None:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+
+                if is_driver(cur_usr):
+	                login(request, user)
+
+	                if is_driver(user):
+	                    return redirect('/drivers', user=user)
+	               	# cur_user_profile = UserProfile.get(id=user.id)
+
+	                profile = get_associated_profile(user)
+	                profile.logins = profile.logins + 1
+	                profile.clicks = profile.clicks + 1
+	                profile.save()
+
+                return redirect('/driver_dash', user=user)
+
+               else:
+               		return HttpResponse("Sorry, a driver profile is required to access this site.")
+
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            #print "Invalid login details: {0}, {1}".format(username, password)
+            return render_to_response('login_error.html', {}, context)
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render_to_response('driver_login.html', {}, context)
+
+
+def is_driver(user):
+    for driver in DriverProfile.objects.all():
+        if driver.user == user:
+            return True
+    return False
+
+
+
+
 def driver_dash(request):
 	context = RequestContext(request)
 
@@ -33,10 +104,6 @@ def driver_dash(request):
 				if dispatch not in dispatch_orders.keys():
 					dispatch_orders[dispatch] = []
 				dispatch_orders[dispatch].append(do)
-
-	
-
-
 
 
 	return render_to_response("driver_home.html", 
