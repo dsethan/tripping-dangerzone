9f3050ae353994be6acf04e4ff7025bfe2219010
Ethan Gottlieb <ethan@dormserv.com>
2014-09-27 16:02:15 -0400

    changed file system

--- dormserv/drivers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from drivers import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.driver_login, name='driver_login'),
-	url(r'^driver_dash', views.driver_dash, name='driver_dash'),
-
-
-    )
