05518f3a002e13a883ccf7cc0096dd1e456b7e70
Ethan Gottlieb <ethan@dormserv.com>
2014-09-28 17:39:12 -0400

    Added kitchen login

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from kitchen import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.kitchen, name='kitchen'),
+	url(r'^$', views.kitchen_login, name='kitchen_login'),
+    url(r'^kitchen_dash', views.kitchen, name='kitchen'),
     )
--- kitchen/views.py
+++ kitchen/views.py
@@ -23,6 +23,52 @@ from time import sleep
 import random
 
 
+def kitchen_login(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+    cur_usr = request.user
+
+    if cur_usr.is_active:
+        if cur_usr.is_staff:
+            return redirect('kitchen_dash')
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user is not None:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+
+                if user.is_staff:
+                    login(request, user)
+                    return redirect('kitchen_dash')
+                else:
+                    return HttpResponse("Sorry, staff only.")
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            #print "Invalid login details: {0}, {1}".format(username, password)
+            return render_to_response('login_error.html', {}, context)
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render_to_response('kitchen_login.html', {}, context)
+
+
 def kitchen(request):
 	context = RequestContext(request)
 	user = request.user
