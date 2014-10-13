9affaf7b581cd2a7a2dd01303f9d2c41ca4d6553
Ethan Gottlieb <ethan@dormserv.com>
2014-10-13 00:26:20 -0400

    change staticfile app

--- dormserv/settings.py
+++ dormserv/settings.py
@@ -110,7 +110,9 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
